29f4275f483fadeb2cd75c964d5f12c61250caaa
Nejeh Zouein <nezouein@espol.edu.ec>
2026-05-13 14:36:50 -0500

    Líder: personalizar botón y colores principales

--- src/main/java/com/espol/taller01/snake/GUIView.java
+++ src/main/java/com/espol/taller01/snake/GUIView.java
@@ -63,7 +63,11 @@ public class GUIView extends JPanel {
 
 		// Create a new button on that panel and add a StartGameListener as
 		// listener on that button
+<<<<<<< HEAD
 		this.startGameButton = new JButton("Let's Go!!!");
+=======
+		this.startGameButton = new JButton("Start Game");
+>>>>>>> parent of 73800eb (LIDER: Personalización de botones y colores base)
 		this.startGameButton.addActionListener(new StartGameListener());
 		this.guiPanel.add(this.startGameButton);
 
